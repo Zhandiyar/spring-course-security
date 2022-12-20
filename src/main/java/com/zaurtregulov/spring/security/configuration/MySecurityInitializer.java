bbf793a929dba845d4fb13bf9cb16e1e389e6639
Zhandiyar <zhandik.1997@mail.ru>
2022-12-20 10:12:55 +0600

    spring security learning

---
+++ src/main/java/com/zaurtregulov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.zaurtregulov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
